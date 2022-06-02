54491838b2225fe41500d85b9060753c97c59090
LT <1061889781@qq.com>
2022-06-02 09:34:31 +0800

    222

---
+++ src/main/java/com/atguigu/java.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * Date:2022/6/2
+ * Author:
+ */
+public class java {
+    public static void main(String[] args) {
+        System.out.println("hello = " + "hello");
+    }
+}
