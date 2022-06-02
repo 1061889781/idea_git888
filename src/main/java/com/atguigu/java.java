6384d2c3c88faf7724469320cc8b756b712b442b
LT <1061889781@qq.com>
2022-06-02 11:29:38 +0800

    第二次更新

--- src/main/java/com/atguigu/java.java
+++ src/main/java/com/atguigu/java.java
@@ -7,5 +7,6 @@ package com.atguigu;
 public class java {
     public static void main(String[] args) {
         System.out.println("hello = " + "hello");
+        System.out.println("第二次更新");
     }
 }
